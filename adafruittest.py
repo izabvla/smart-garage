0b09242f311be69fe9657df68cd590eb3df65f40
iotgroup3 <117319203+iotgroup3@users.noreply.github.com>
2022-11-17 19:08:09 -0800

    Update adafruittest.py

--- adafruittest.py
+++ adafruittest.py
@@ -2,15 +2,16 @@
 
 from Adafruit_IO import RequestError, Client, Feed
 
-ADAFRUIT_IO_USERNAME = ''
-ADAFRUIT_IO_KEY = ''
+ADAFRUIT_IO_USERNAME = 'compiotgroup3'
+ADAFRUIT_IO_KEY = 'aio_gpXs67Bn7HYZmTFj0t1Zuisebnv5'
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
+# Try to assign test to existing feed 'test'
 try:
     test = aio.feeds('test')
 except RequestError: # Doesn't exist, create new feed
     test_feed = Feed(name='test')
-    test_feed = aio.create_feed(feed)
+    test_feed = aio.create_feed(test_feed)
 
-aio.send_data(test.key, 42)
+aio.send_data(test.key, 32)
