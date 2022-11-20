bd778e937bd9c4ebd0e2519b61f22223a82061e2
iotgroup3 <117319203+iotgroup3@users.noreply.github.com>
2022-11-20 15:27:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-# main.py
-
 import RPi.GPIO as GPIO
 import time
-import keyboard
+import board
+import adafruit_mpu6050
+
+# GY-521
+i2c = board.I2C()  # uses board.SCL and board.SDA
+mpu = adafruit_mpu6050.MPU6050(i2c)
 
 GPIO.setwarnings(False)
-# GPIO.BOARD ->
-#   1 2
-#   3 4
-#   5 ...
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
-# PIR input, pin 8
-GPIO.setup(8, GPIO.IN, GPIO.PUD_DOWN)
-# LED output, pin 10
-GPIO.setup(10, GPIO.OUT)
-# Buzzer alarm output, pin 11
-GPIO.setup(11, GPIO.OUT)
+# PIR input
+GPIO.setup(14, GPIO.IN, GPIO.PUD_DOWN)
+PIR_PIN = 14
+# LED output
+GPIO.setup(15, GPIO.OUT)
+LED_PIN = 15
+# Buzzer output
+GPIO.setup(17, GPIO.OUT)
+BUZZER_PIN = 17
 
 # Import Adafruit_IO library and create instance of REST client.
 from Adafruit_IO import RequestError, Client, Feed
@@ -26,74 +28,73 @@ ADAFRUIT_IO_KEY = 'aio_gpXs67Bn7HYZmTFj0t1Zuisebnv5'
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-# Establishing connection to Adafruit feeds
-# Motion LED
+# connecting/creating adafruit feeds
 try:
-    ledFeed = aio.feeds('motion-light')
-except RequestError: # Doesn't exist, create new feed
+    led_feed = aio.feeds('motion-light')
+except RequestError:
     led_feed = Feed(name='motion-light')
     led_feed = aio.create_feed(led_feed)
-# PIR HC-SR501 Motion sensor
+    
 try:
     motion_feed = aio.feeds('hc-sr501')
 except RequestError:
     motion_feed = Feed(name='hc-sr501')
     motion_feed = aio.create_feed(motion_feed)
-# Buzzer alarm sensor
+    
 try:
     alarm_feed = aio.feeds('alarm-feed')
 except RequestError:
     alarm_feed = Feed(name='alarm-feed')
     alarm_feed = aio.create_feed(alarm_feed)
 
-def motion_detected():
-    # motion: 1 - motion detected, 0 - no motion
-    motion = GPIO.input(8)
-    return True if motion == 1 else False
+try:
+    temperature_feed = aio.feeds('temperature')
+except RequestError:
+    temperature_feed = Feed(name='temperature')
+    temperature_feed = aio.create_feed(temperature_feed)
 
-def alarm_activated():
-    # alarm: 1 - alarm on, 0 - alarm off
-    alarm = aio.receive('alarm-feed').value
-    return True if alarm == 1 else False
-
-
-def main():
-
-    print('Smart Garage system is online. Press any key to start.')
-    usr_in = input(':::')
-
-    # While user input is not [Q] - Quit
-    while usr_in.lower() is not 'q':
-            
-        # If any keyboard input -> press any key to resume, or Q to terminate
-        if keyboard.is_pressed():
-            print('Press any key to start, or [Q] - Quit')
-            usr_in = input(':::')
-            if usr_in.lower() is 'q':
-                print('Smart Garage system is going offline.')
-                break
-        
-        _motion = motion_detected()
-        _alarm = alarm_activated()
-        message = ""
-        if _motion:
-            if _alarm:
-                # Turn on motion light and buzzer alarm
-                GPIO.output(10,1)
-                GPIO.output(11,GPIO.HIGH)
-                time.sleep(0.2)
-                GPIO.output(11, GPIO.LOW)
-                message = 'Motion detected. Intruder alert.'
-            # Turn on motion light and buzzer alarm
-            GPIO.output(10,1)
-            message = 'Motion detected.'
-        else:
-            message = 'No motion detected.'
-        print(message)
-
-        # Send data to Adafruit dashboards
-        aio.send_data(led_feed.key, _motion)
+#receiving and sending data
+while True:
+    # Motion status (ON -1, OFF - 0)
+    motion = GPIO.input(PIR_PIN)
+    # Alarm status (ON -1, OFF - 0)
+    alarm_status = aio.receive('alarm-feed').value
+    # Temperature
+    temperature = mpu.temperature
+    
+    #
+    message = ''
+    
+    # if no motion detected (input is 0)
+    if motion == 0:
+        message = 'No motion detected.'
+        aio.send_data(led_feed.key, motion)
         aio.send_data(motion_feed.key, message)
+        GPIO.output(LED_PIN,0)
+        time.sleep(2)
+        print(message)
+    # if motion detected
+    else:
+        # LED ON
+        GPIO.output(LED_PIN,1)
+        # if alarm is on
+        if alarm_status == '1':
+            message = 'Motion detected. Intruder alert.'
+            GPIO.output(BUZZER_PIN,GPIO.HIGH)
+            time.sleep(0.1)
+            GPIO.output(BUZZER_PIN, GPIO.LOW)
+            print(message)
+        # else if alarm is off
+        else:
+            message = 'Motion detected.'
+            GPIO.output(BUZZER_PIN,GPIO.LOW)
+            print(message)
 
-if __name__ == "__main__":
-    main()
+        aio.send_data(led_feed.key, motion)
+        aio.send_data(motion_feed.key, message)
+        
+    aio.send_data(temperature_feed.key, temperature)
+    time.sleep(2)
+    # print("Temperature: %.2f C" % temperature)
+
+
