7b76424a17a1b26c034080c8bcfddd5a387f8e6e
iotgroup3 <117319203+iotgroup3@users.noreply.github.com>
2022-11-20 16:06:28 -0800

    Add files via upload

--- tests/keypad-test.py
+++ tests/keypad-test.py
@@ -27,23 +27,38 @@ GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def readLine(line, characters):
+def readLine(line, characters, pwd):
     GPIO.output(line, GPIO.HIGH)
     if(GPIO.input(C1) == 1):
-        print(characters[0])
+        pwd.append(characters[0])
     if(GPIO.input(C2) == 1):
-        print(characters[1])
+        pwd.append(characters[1])
     if(GPIO.input(C3) == 1):
-        print(characters[2])
+        pwd.append(characters[2])
     if(GPIO.input(C4) == 1):
-        print(characters[3])
+        pwd.append(characters[3])
     GPIO.output(line, GPIO.LOW)
+    
 try:
     while True:
-        readLine(L1, ["1","2","3","A"])
-        readLine(L2, ["4","5","6","B"])
-        readLine(L3, ["7","8","9","C"])
-        readLine(L4, ["*","0","#","D"])
-        time.sleep(0.1)
+        PASSWORD = ['1','1','1','1']
+        pwd = []
+        while len(pwd) != 4:
+            readLine(L1, ["1","2","3","A"], pwd)
+            readLine(L2, ["4","5","6","B"], pwd)
+            readLine(L3, ["7","8","9","C"], pwd)
+            readLine(L4, ["*","0","#","D"], pwd)
+            time.sleep(0.1)
+        print(pwd)
+        print(PASSWORD)
+        for i in range(4):
+            if pwd[i] == PASSWORD[i]:
+                continue
+                if pwd[3] == PASSWORD[3]:
+                    print('Righ password.')
+                    break
+            else:
+                print('Wrong password.')
+                break
 except KeyboardInterrupt:
     print("\nApplication stopped!")
